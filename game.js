fa6326928d0d2ccba206ebb8766e9947937b6e2e
znataniel <nrojaszavalla@frba.utn.edu.ar>
2024-01-07 22:23:10 -0300

    add fn to check if the game has ended.

--- game.js
+++ game.js
@@ -1,29 +1,80 @@
-const gameBoard = (function (boardSelector) {
-  let boardSpaces = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+const board = (function (boardSelector) {
+  let spaces = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let move = 0;
 
   const getBoard = function () {
     console.log(`
       ######## BOARD ########\n
-      ${boardSpaces[0]} ${boardSpaces[1]} ${boardSpaces[2]}\n
-      ${boardSpaces[3]} ${boardSpaces[4]} ${boardSpaces[5]}\n
-      ${boardSpaces[6]} ${boardSpaces[7]} ${boardSpaces[8]}\n
+      ${spaces[0]} ${spaces[1]} ${spaces[2]}\n
+      ${spaces[3]} ${spaces[4]} ${spaces[5]}\n
+      ${spaces[6]} ${spaces[7]} ${spaces[8]}\n
       ######## BOARD ########\n
     `);
   };
 
-  const setSpace = function (index, playerID) {
-    if (index > 0 && index < 10) {
-      boardSpaces[index - 1] = playerID;
+  const setSpace = function (space, playerId) {
+    if (space >= 0 && space < 9) {
+      if (!spaces[space]) {
+        spaces[space] = playerId;
+        console.log(`Player ${playerId} played in space ${space}`);
+        move++;
+      } else {
+        console.log(`Space ${space} is already occupied.`);
+      }
+      getBoard();
+      if (move > 4) {
+        if (!checkWin()) {
+          console.log(`Game finished! Player ${playerId} wins!!`);
+        } else {
+          console.log("Game keeps going.");
+        }
+        if (move == 9) {
+          console.log(`Game finished! It's a tie.`);
+        }
+      }
     }
   };
 
   const resetBoard = function () {
-    for (let i in boardSpaces) {
-      boardSpaces[i] = 0;
+    for (let i in spaces) {
+      spaces[i] = 0;
     }
+    move = 0;
+    console.log("Resetting board");
+    getBoard();
   };
 
-  const checkWin = function () {};
+  const checkWin = function () {
+    // Check rows
+    [0, 3, 6].forEach((i) => {
+      if (spaces[i] == spaces[i + 1] && spaces[i + 1] == spaces[i + 2]) {
+        if (spaces[i]) {
+          return spaces[i];
+        }
+      }
+    });
+
+    // Check columns
+    [0, 1, 2].forEach((i) => {
+      if (spaces[i] == spaces[i + 3] && spaces[i + 3] == spaces[i + 6]) {
+        if (spaces[i]) {
+          return spaces[i];
+        }
+      }
+    });
+
+    // Check diagonals
+    if (
+      (spaces[0] == spaces[4] && spaces[4] == spaces[8]) ||
+      (spaces[2] == spaces[4] && spaces[4] == spaces[6])
+    ) {
+      if (spaces[4]) {
+        return spaces[4];
+      }
+    }
+
+    return 0;
+  };
 
   return {
     getBoard,
@@ -35,4 +86,6 @@ const gameBoard = (function (boardSelector) {
 function Player(name, id) {
   const playerName = name;
   const playerId = id;
+
+  return { playerName, playerId };
 }
